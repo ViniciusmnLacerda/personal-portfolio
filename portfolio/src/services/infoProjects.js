b98e101b855c042d19a8b3fcd68e886946c92ac5
Vinicius Lacerda <viniciuslmn@gmail.com>
2022-09-02 17:11:41 -0300

    add music player project

--- portfolio/src/services/infoProjects.js
+++ portfolio/src/services/infoProjects.js
@@ -8,12 +8,20 @@ const infoProjects = [
     techlist: ['VS code', 'ReactJS', 'Responsive', 'Redux'],
   },
   {
-    title: 'Top Trump',
+    title: 'Music Player',
     description: 'In this application it is possible to create cards from the game Top Trumps, very famous in the 90s. This app is be able to: Fill in the form with the information of each card; Select a card to be Top Trump; View all created cards; Filter cards by rarity, name or Top Trump.',
+    image: '/music-app.png',
+    pathToApp: 'https://music-player-henna-gamma.vercel.app/',
+    pathToGitHub: 'https://github.com/ViniciusmnLacerda/music-player',
+    techlist: ['VS code', 'ReactJS', 'Responsive', 'CSS'],
+  },
+  {
+    title: 'Top Trump',
+    description: 'Lorem ipsum dolor sit amet. Ab aspernatur nisi et impedit aperiam et optio quia quo odit eius cum temporibus doloribus. Aut consectetur ipsam in quia laudantium aut veritatis provident ea magnam dolor et distinctio assumenda ut quidem incidunt aut quas eaque? ',
     image: '/tryunfo.png',
     pathToApp: 'https://top-trumps.vercel.app/',
     pathToGitHub: 'https://github.com/ViniciusmnLacerda/top-trumps',
-    techlist: ['VS code', 'ReactJS', 'Responsive'],
+    techlist: ['VS code', 'ReactJS', 'Responsive', 'CSS'],
   },
 ];
 
