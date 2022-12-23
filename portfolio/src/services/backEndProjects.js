47736a2e094bd0760f047e4234315c7873850e3b
Vinícius Lacerda <viniciuslmn@gmail.com>
2022-12-23 12:26:20 -0300

    add chat-room project

--- portfolio/src/services/backEndProjects.js
+++ portfolio/src/services/backEndProjects.js
@@ -1,5 +1,13 @@
 const backEndProjects = [
   {
+    title: 'Chat Room',
+    description: 'This project is a conversation application. It is possible to create an account, start a conversation with any user who is registered, send messages and edit your profile.',
+    image: '/chat-room.png',
+    pathToApp: 'https://github.com/ViniciusmnLacerda/chat-room',
+    pathToGitHub: 'https://github.com/ViniciusmnLacerda/chat-room',
+    techlist: ['Node.js', 'TypeScript', 'Sequelize', 'MySQL', 'React', 'Docker'],
+  },
+  {
     title: 'Trasfers App',
     description: `This project is a transfer application. Backend was developed with Node.js, the database is PostgreSQL, built with the help of ORM Prisma.
     Frontend was developed with React, ContextAPI to manage the state and CSS to make the styles.`,
